203dba0d720c8a00f1a75512cfe54b9679a95390
est3banst <estebeeen@outlook.com>
2025-03-17 01:19:06 -0300

    clases added

--- src/componentes/ClasesCard.jsx
+++ src/componentes/ClasesCard.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 
-const ClasesCard = () => {
+const ClasesCard = ({ src, title, desc, time, diff, inst }) => {
   return (
-    <div>
-        <section>
-            <p></p>
-            <p></p>
-        </section>
+    <div className='w-full relative'>
+      <div className="absolute inset-0 bg-gradient-to-t from-black/90 to-transparent rounded-lg"></div>
+
+      <section>
+        <img className="w-full h-full object-cover rounded-lg" src={src} alt={title}/>
+        <p>{title}</p>
+        <p>{desc}</p>
+        <article>
+          <p>
+            {time}
+          </p>
+          <p>
+            {diff}
+          </p>
+        </article>
+        <article>
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+              <g fill="none" stroke="currentColor" strokeWidth={2}>
+                <circle cx={12} cy={7} r={5}></circle>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17 14h.352a3 3 0 0 1 2.976 2.628l.391 3.124A2 2 0 0 1 18.734 22H5.266a2 2 0 0 1-1.985-2.248l.39-3.124A3 3 0 0 1 6.649 14H7"></path>
+              </g>
+            </svg>
+          </span>
+          <h2>
+            {inst}
+          </h2>
+        </article>
+      </section>
     </div>
   )
 }
--- src/componentes/pages/ClasesLayout.jsx
+++ src/componentes/pages/ClasesLayout.jsx
@@ -1,18 +1,72 @@
 import React from 'react'
 import Nav from '../Nav'
+import ClasesCard from '../ClasesCard'
+import Footer from '../Footer'
 
 const ClasesLayout = () => {
+
+  const clases = [
+    {
+      title: 'HIIT',
+      desc: 'Entrenamiento de alta intensidad en intervalos, nada quema más calorías y mejora tu salud cardiovascular',
+      inst: 'Diego Lopez',
+    },
+    {
+      title: 'Pilates',
+      desc: 'Pilates con un enfoque moderno en el que incorporarás movimientos dinámicos y entrenamientos de resistencia',
+      inst: 'Victoria Rodriguez',
+    },
+    {
+      title: 'Boxeo',
+      desc: 'Aprende la técnica correcta del boxeo mientras aumentas tu agilidad y tu resistencia',
+      inst: 'Marcos Maidana',
+    },
+    {
+      title: 'Powerlifting',
+      desc: 'Aumenta tu fuerza y tu resistencia con entrenamientos pesados y ejercicios compuestos',
+      inst: 'Eddie Hall',
+    },
+    {
+      title: 'Crossfit Elite',
+      desc: 'Supera tus límites con nuestro entrenamiento funcional de alta intensidad combinando movimientos gimnasticos, pesas y cardio',
+      inst: 'Martin Danm',
+    },
+    {
+      title: 'Beast mode',
+      desc: 'Incrementa tu fuerza y tonifica tu cuerpo con nuestros entrenamientos de alta intensidad aplicando el principio de sobrecarga progresiva',
+      inst: 'Pablo Itrue',
+    },
+    {
+      title: 'Yoga',
+      desc: 'Conecta tu mente con tu cuerpo con movimientos dinámicos que mejoran tu postura y disminuyen tu estrés',
+      inst: 'Diana Urie'
+    }
+  ]
   return (
     <>
     <Nav/>
     <div className='bg-[url(/clases-bg.jpg)] relative bg-no-repeat bg-cover bg-center h-screen'>
     <div className="absolute inset-0 bg-gradient-to-t from-black/90 to-transparent"></div>
-   
+    <span className='flex w-full items-center h-full p-2'>
+      <h2 className='anton uppercase relative text-6xl md:text-7xl lg:text-8xl'>
+        Nuestras <b className='text-amber-600'>clases</b>
+      </h2>
+      </span>
     </div>
     <div className="absolute inset-0 custom-clases">
     <div className='w-full inset-0 border-2 border-slate-50 bg-amber-50' 
          style={{ clipPath: 'polygon(100% 51%, 100% 84%, 100% 100%, 0% 100%, 50% 88%, 80% 75%)', height: '2px', borderTop: '2px solid #f8fafc' }}></div>    
          </div>   
+    <div className='my-4 grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
+      {clases.map((item, idx) => (
+        <ClasesCard 
+        title={item.title}
+        desc={item.desc}
+        inst={item.inst}/>
+
+      ))}
+    </div>
+    <Footer/>
     </>
   )
 }
